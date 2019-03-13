b036b59f8745d2f33a9717325086cf7419a2fb75
Preston Tunnell Wilson <preston_tunnell_wilson@brown.edu>
2019-03-13 14:21:22 -0400

    start adding pyret tests

---
+++ spec/pyret-lang-integration-test.js
@@ -0,0 +1,35 @@
+import CodeMirrorBlocks from '../src/CodeMirrorBlocks';
+import pyret from '../src/languages/pyret';
+import { store } from '../src/store';
+
+import { click, keyDown, insertText } from './support/simulate';
+import TestUtils from 'react-dom/test-utils';
+import { wait, cleanupAfterTest } from './support/test-utils';
+
+const DELAY = 500;
+
+describe('Pyret-lang integration', function () {
+  beforeEach(function () {
+    const fixture = `<div id="root">
+    <div id="cmb-editor" class="editor-container" />
+    </div>`;
+    document.body.insertAdjacentHTML('afterbegin', fixture);
+    const container = document.getElementById('cmb-editor');
+    this.cmb = new CodeMirrorBlocks(container, { value: "" }, pyret);
+    this.cmb.setBlockMode(true);
+    this.toggle = document.getElementsByClassName('blocks-toggle-btn')[0];
+    console.log(this.toggle);
+  });
+
+  afterEach(function () {
+    cleanupAfterTest('root', store);
+  });
+
+  it('should have the same text after hitting toggle', async function () {
+    insertText('3 + 5');
+    await wait(DELAY);
+    click(this.toggle);
+    await wait(DELAY);
+    expect(this.cmb.getValue()).toBe('3 + 5');
+  })
+})
